9355e1731e62bfef411791b517bf93fd56a5ad6f
Jules <julesmoreno66@gmail.com>
2026-01-20 13:59:45 +0800

    update: the can now move left and right

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -29,14 +29,16 @@ class AlienInvasion:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        # Move the ship to the right.
+                        # Move the ship to left and right
                         self.ship.moving_right = True
                     elif event.key == pygame.K_LEFT:
-                        self.ship.rect.x -= 20
+                        self.ship.moving_left = True
                 
-                elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
-                    # if event.key == pygame.K_RIGHT:
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
