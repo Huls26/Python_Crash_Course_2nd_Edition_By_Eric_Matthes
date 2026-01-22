62a27a1daa02f075f70ce503086481e425798589
Jules <julesmoreno66@gmail.com>
2026-01-22 14:55:38 +0800

    update bullet

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -14,13 +14,25 @@ class Bullet:
 
         self.bullet_rect.midbottom = self.ship.rect.midtop
         
+        self.y = float(self.bullet_rect.y)
         self.shoot_bullet = False
 
     def update(self):
         if self.shoot_bullet:
+            self.y -= self.setting.bullet_speed
+         
+            self.bullet_rect.x = self.ship.rect.midtop[0]
+            self.bullet_rect.y = self.y
+
             pygame.draw.rect(
                 self.screen,
                 self.setting.bullet_color,
                 self.bullet_rect
             )    
+
+        if not self.shoot_bullet:
+            self.y = self.ship.rect.midtop[1]
+
+
+            
         
