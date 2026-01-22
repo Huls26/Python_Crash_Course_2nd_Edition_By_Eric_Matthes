a44e23cee6f0136168e84b43529c536a511146cb
Jules <julesmoreno66@gmail.com>
2026-01-22 18:20:08 +0800

    created _update_bullets() helper function

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -71,18 +71,19 @@ class AlienInvasion:
         # Make the most recently drawn screen visible.                        
         pygame.display.flip()
          
+    def _update_bullets(self):
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.bullets.update()
             self.ship.update()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self._update_bullets()
             self._update_screen()
                 
 if __name__ == '__main__':
