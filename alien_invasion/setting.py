335ac5f9c1db2e999a23aa135aa352119bda0ff1
Jules <julesmoreno66@gmail.com>
2026-01-26 19:17:46 +0800

    update aliens speed

--- alien_invasion/setting.py
+++ alien_invasion/setting.py
@@ -12,8 +12,8 @@ class Setting:
         self.ship_speed = 0.5
 
         # Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 5
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
