04079b6cc3a90d96ff22fceda00be1ed4a190dcc
Jules <julesmoreno66@gmail.com>
2026-01-27 14:22:29 +0800

    change bullet size

--- chapter_12/sideways_shooter_12_6.py
+++ chapter_12/sideways_shooter_12_6.py
@@ -71,8 +71,8 @@ class Bullet(Sprite):
 
          # Bullet settings
         self.bullet_speed = 0.5
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 5
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
