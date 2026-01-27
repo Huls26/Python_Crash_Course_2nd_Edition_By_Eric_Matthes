199b252564fda7fa2eb3adcf452d644c4a0441f1
Jules <julesmoreno66@gmail.com>
2026-01-27 11:56:21 +0800

    update comment

--- chapter_13/raindrops_13_3/rain.py
+++ chapter_13/raindrops_13_3/rain.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Rain(Sprite):
-    """A class to represent a single star."""
+    """A class to represent a single raindrop."""
 
     def __init__(self, game):
         super().__init__()
@@ -11,13 +11,13 @@ class Rain(Sprite):
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
 
-        # Load the star image from the images folder
+        # Load the rain image from the images folder
         self.rain = pygame.image.load('../images/raindrop.bmp')
         
         # Scale the image to a fixed size (50x50 pixels)
         self.image = pygame.transform.scale(self.rain, (50, 50))
 
-        # Get a rect object for positioning the star
+        # Get a rect object for positioning the raindrop
         self.rect = self.image.get_rect()
         
         # Set an initial position (not final grid position yet)
@@ -29,6 +29,6 @@ class Rain(Sprite):
         self.y = float(self.rect.y)
     
     def update(self):
-        """Move the raindrop to the down."""
+        """Move the raindrop down the screen"""
         self.y += self.raindrop_speed
         self.rect.y = self.y
