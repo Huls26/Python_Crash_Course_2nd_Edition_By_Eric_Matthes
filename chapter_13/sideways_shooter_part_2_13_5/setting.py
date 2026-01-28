611b77524cdfa1fc8989ab1ef5a66dfeb8d803ba
Jules <julesmoreno66@gmail.com>
2026-01-28 13:46:48 +0800

    update screen_height

--- chapter_13/sideways_shooter_part_2_13_5/setting.py
+++ chapter_13/sideways_shooter_part_2_13_5/setting.py
@@ -5,7 +5,7 @@ class Setting:
         """Initialize the game's settings."""
         # Screen set
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 870
         self.bg_color = (255, 255, 255)
 
         # ship speed
