7c1c412cdb8c0457ebf8eff9f759df38c3f9fa8c
Jules <julesmoreno66@gmail.com>
2026-01-28 15:00:44 +0800

    renamee instance

--- chapter_13/sideways_shooter_part_2_13_5/setting.py
+++ chapter_13/sideways_shooter_part_2_13_5/setting.py
@@ -13,7 +13,7 @@ class Setting:
 
         # Alien settings
         self.alien_speed = 5
-        self.fleet_speed = 10
+        self.fleet_move_left = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
