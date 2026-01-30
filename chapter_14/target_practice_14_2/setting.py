6af1569ce70f26c560482ae341e10fab6dc324c9
Jules <julesmoreno66@gmail.com>
2026-01-30 13:47:18 +0800

    feat: update Target color in settings

--- chapter_14/target_practice_14_2/setting.py
+++ chapter_14/target_practice_14_2/setting.py
@@ -18,6 +18,7 @@ class Setting:
         self.max_alien_hits = 15
         self.target_width = 50
         self.target_height = 75
+        self.target_color = (3, 26, 96)
 
         # Bullet settings
         self.bullet_speed = 10
