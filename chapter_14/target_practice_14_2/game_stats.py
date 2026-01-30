ab78fb8ac8d80fba7fbb66d55e5da7e955c11092
Jules <julesmoreno66@gmail.com>
2026-01-30 19:09:39 +0800

    refactor: rename self.settings to setting for consistency

--- chapter_14/target_practice_14_2/game_stats.py
+++ chapter_14/target_practice_14_2/game_stats.py
@@ -8,7 +8,7 @@ class GameStats:
         Args:
             ai_game: An instance of the Game class, used to access settings.
         """
-        self.settings = ai_game.setting
+        self.setting = ai_game.setting
 
         # Start the game in an active state
         self.game_active = False
@@ -20,6 +20,6 @@ class GameStats:
         """Initialize statistics that can change during the game."""
         # Track the number of target hits and ship hits
         # Number of ships remaining
-        self.ships_left = self.settings.ship_limit
+        self.ships_left = self.setting.ship_limit
         self.target_hits = 0
         self.bullets_missed_count = 0
