c5a13dbb483f2ef09b7464aa4b37e3e873b77117
Jules <julesmoreno66@gmail.com>
2026-02-03 15:55:43 +0800

    feat: add scoring for alien hits and update scoreboard

--- alien_invasion/setting.py
+++ alien_invasion/setting.py
@@ -36,3 +36,6 @@ class Setting:
         self.alien_speed = 1.0
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
