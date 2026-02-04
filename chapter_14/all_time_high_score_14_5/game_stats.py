c934690ac903bfa0340783d93815aa828332f3da
Jules <julesmoreno66@gmail.com>
2026-02-04 18:18:01 +0800

    feat: load high score from file

--- chapter_14/all_time_high_score_14_5/game_stats.py
+++ chapter_14/all_time_high_score_14_5/game_stats.py
@@ -6,14 +6,20 @@ class GameStats:
         self.settings = ai_game.setting
         # Start Alien Invasion in an active state.
         self.game_active = False
-        
-        # High score should never be reset.
-        self.high_score = 0
 
         self.reset_stats()
+        self.load_high_score()
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
+    
+    def load_high_score(self):
+        """Load high score from file."""
+        try:
+            with open("high_score.txt", "r", encoding='utf-8') as file:
+                self.high_score = int(file.read())
+        except (FileNotFoundError, ValueError):
+            self.high_score = 0
