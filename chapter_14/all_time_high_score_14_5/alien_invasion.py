ab5eef85902aa3593b40a6538a7e21b1e4ddf8f1
Jules <julesmoreno66@gmail.com>
2026-02-05 11:24:50 +0800

    fix: count multiple bullet hits on aliens

--- chapter_14/all_time_high_score_14_5/alien_invasion.py
+++ chapter_14/all_time_high_score_14_5/alien_invasion.py
@@ -182,7 +182,8 @@ class AlienInvasion:
                         self.bullets, self.aliens, True, True)
         
         if collisions:
-            self.stats.score += self.setting.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
             self.sb.check_high_score()
 
