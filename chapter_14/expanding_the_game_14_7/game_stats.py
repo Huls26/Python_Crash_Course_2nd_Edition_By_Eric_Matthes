f9347d85577c42989c4ffb3900f28a7446eedfca
Jules <julesmoreno66@gmail.com>
2026-02-06 10:42:33 +0800

    chore: duplicate files for activity 14-7

---
+++ chapter_14/expanding_the_game_14_7/game_stats.py
@@ -0,0 +1,32 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.setting
+        # Start Alien Invasion in an active state.
+        self.game_active = False
+
+        self.reset_stats()
+        self.load_high_score()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
+    
+    def load_high_score(self):
+        """Load high score from file."""
+        try:
+            with open("high_score.txt", "r", encoding='utf-8') as file:
+                self.high_score = int(file.read())
+        except (FileNotFoundError, ValueError):
+            self.high_score = 0
+       
+    def save_high_score(self):
+        try:
+            with open("high_score.txt", "w", encoding='utf-8') as file:
+                file.write(str(self.high_score))
+        except (TypeError, PermissionError, OSError) as e:
+            print(f"Failed to save high score: {e}")
