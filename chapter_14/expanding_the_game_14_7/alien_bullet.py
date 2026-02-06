ba4badf5910eca5aecfefd501d80772491b3dd8f
Jules <julesmoreno66@gmail.com>
2026-02-06 11:35:25 +0800

    feat: create alien bullet class

---
+++ chapter_14/expanding_the_game_14_7/alien_bullet.py
@@ -0,0 +1,17 @@
+from bullet import Bullet
+
+class AlienBullet(Bullet):
+    """A class to manage bullets fired by aliens."""
+
+    def __init__(self, ai_game, alien):
+        super().__init__(ai_game)
+        self.settings = ai_game.setting
+
+        # Override starting position
+        self.rect.midbottom = alien.rect.midbottom
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet down the screen."""
+        self.y += self.settings.bullet_speed
+        self.rect.y = self.y
