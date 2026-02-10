752720c013df5b8f00e0a5a4ed778ab841214d4f
Jules <julesmoreno66@gmail.com>
2026-02-10 14:20:19 +0800

    refactor: adjust dynamic game settings values

--- chapter_14/sideways_shooter_final_14_8/setting.py
+++ chapter_14/sideways_shooter_final_14_8/setting.py
@@ -9,16 +9,24 @@ class Setting:
         # White background
         self.bg_color = (255, 255, 255)
 
-        # Ship settings
-        self.ship_speed = 15
         self.ship_limit = 3
 
-        # Alien settings
-        self.alien_speed = 0.1
         self.max_alien_hits = 15
 
         # Bullet settings
-        self.bullet_speed = 10
         self.bullet_width = 15
         self.bullet_height = 5
         self.bullet_color = (60, 60, 60)
+
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 15
+        self.bullet_speed = 10
+        self.alien_speed = 1.0
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
