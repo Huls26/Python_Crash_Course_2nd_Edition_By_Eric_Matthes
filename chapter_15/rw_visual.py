28fd7453b25e260bc86729a3b41662cbe0ccf9b4
Jules <julesmoreno66@gmail.com>
2026-02-11 19:46:00 +0800

    feat: set custom figure size for random walk plot

--- chapter_15/rw_visual.py
+++ chapter_15/rw_visual.py
@@ -4,12 +4,12 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9))
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
         edgecolors='none', s=15)
