7394f439054eadc15ece064c9780a3a833846b76
Jules <julesmoreno66@gmail.com>
2026-02-11 14:58:19 +0800

    feat: add scatter plot with colormap based on y-values

--- chapter_15/scatter_squares.py
+++ chapter_15/scatter_squares.py
@@ -6,7 +6,8 @@ y_values = [x**2 for x in x_values]
 # print(plt.style.available)
 plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
+
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
