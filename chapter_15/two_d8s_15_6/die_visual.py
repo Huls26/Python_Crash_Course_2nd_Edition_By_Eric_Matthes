9b78d68a3b0a901ca6218da5c5dac431bf9ba7e5
Jules <julesmoreno66@gmail.com>
2026-02-15 15:59:45 +0800

    fix: count dice sums starting from 2

--- chapter_15/two_d8s_15_6/die_visual.py
+++ chapter_15/two_d8s_15_6/die_visual.py
@@ -16,7 +16,7 @@ for roll_num in range(10_000_000):
 # Analyze the results.
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(1, max_result+1):
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
