ca52658ca8ec890a4754d253003b107f1f56e9dd
Jules <julesmoreno66@gmail.com>
2026-02-15 15:24:32 +0800

    feat: increase dice roll simulation to 10,000,000 iterations

--- chapter_15/two_d8s_15_6/die_visual.py
+++ chapter_15/two_d8s_15_6/die_visual.py
@@ -9,7 +9,7 @@ die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(10_000_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,6 +26,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D8 dice 1000 times',
+my_layout = Layout(title='Results of rolling two D8 dice 10,000,000 times',
  xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
