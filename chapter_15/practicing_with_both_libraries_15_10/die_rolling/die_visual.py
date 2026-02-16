4252390e04bd8f8a5cbc4ad2083e2d22b183b4dd
Jules <julesmoreno66@gmail.com>
2026-02-16 13:58:10 +0800

    feat: visualize dice roll results with matplotlib

--- chapter_15/practicing_with_both_libraries_15_10/die_rolling/die_visual.py
+++ chapter_15/practicing_with_both_libraries_15_10/die_rolling/die_visual.py
@@ -13,27 +13,24 @@ results = [die_1.roll() + die_2.roll() for _ in range(1_000)]
 max_result = die_1.num_sides + die_2.num_sides
 frequencies = [results.count(value) for value in range(2, max_result+1)]
 
-plt.style.use('_mpl-gallery')
-
 # Visualize the results.
 x_values = list(range(2, max_result+1))
 
-# x_axis_config = {'title': 'Result', 'dtick': 1}
-# y_axis_config = {'title': 'Frequency of Result'}
-# my_layout = Layout(title='Results of rolling two D6 dice 1,000 times',
-#  xaxis=x_axis_config, yaxis=y_axis_config)
-
+plt.style.use('classic')
 # plot
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(15, 9))
 
 ax.bar(x_values, frequencies, width=1, edgecolor="white", linewidth=5)
 
 # Set chart title and label axes.
 ax.set_title("Results of rolling two D6 dice 1,000 times", fontsize=24)
-ax.set_xlabel("result", fontsize=14)
-ax.set_ylabel("Frequency of Result", fontsize=14)
+ax.set_xlabel("result", fontsize=20)
+ax.set_ylabel("Frequency of Result", fontsize=20)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)
+ax.tick_params(axis='both', labelsize=20)
+
+# Set the range for each axis.
+ax.axis([1, max_result + 1, 0, max(frequencies) + 50])
 
 plt.show()
