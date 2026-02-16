61bce7000147c8254e1675347b6eae0a390ee0e9
Jules <julesmoreno66@gmail.com>
2026-02-17 07:45:58 +0800

    feat: parse high temperature values from weather dataset

--- chapter_16/sitka_highs.py
+++ chapter_16/sitka_highs.py
@@ -4,5 +4,11 @@ filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
-    print(list(reader))
+    
+    # Get high temperatures from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+        
+print(highs)
