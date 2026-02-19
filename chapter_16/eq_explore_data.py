f1a77076cb24d2159baf31bd9c5fc26208fa08fe
Jules <julesmoreno66@gmail.com>
2026-02-19 14:39:37 +0800

    feat: parse magnitude and coordinates from GeoJSON

--- chapter_16/eq_explore_data.py
+++ chapter_16/eq_explore_data.py
@@ -9,9 +9,14 @@ readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
 
-mags = []
+mags, lons, lats = [], [], []
 for data in all_eq_data['features']:
     mag = data['properties']['mag']
-    mags.append(float(mag))
+    lon = data['geometry']['coordinates'][0]
+    lat = data['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags)
+print(all_eq_data)
