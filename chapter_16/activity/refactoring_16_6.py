e8e35ec83b4d88d5177f599913105d60e22ef484
Jules <julesmoreno66@gmail.com>
2026-02-25 13:42:53 +0800

    refactor: simplify earthquake data extraction loop

---
+++ chapter_16/activity/refactoring_16_6.py
@@ -0,0 +1,36 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+# Explore the structure of the data.
+filename = '../data/eq_data_30_day_m1.json'
+with open(filename, encoding="utf-8") as f:
+    all_eq_data = json.load(f)
+
+mags, lons, lats, hover_texts = [], [], [], []
+for data in all_eq_data['features']:
+    if data['properties']['mag'] is not None:
+        mags.append(data['properties']['mag'])
+        lons.append(data['geometry']['coordinates'][0])
+        lats.append(data['geometry']['coordinates'][1])
+        hover_texts.append(data['properties']['title'])
+
+# Map the earthquakes.
+data = [{
+        'type': 'scattergeo',
+        'lon': lons,
+        'lat': lats,
+        'text': hover_texts,
+        'marker': {
+            'size': [3 * abs(mag) for mag in mags],
+            'color': mags,
+            'colorscale': 'Viridis',
+            'reversescale': True,
+            'colorbar': {'title': 'Magnitude'},
+        }
+    }]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
