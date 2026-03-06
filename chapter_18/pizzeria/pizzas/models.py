3eacc5062c2eead886d7cb3b921dfab10e0442ef
Jules <julesmoreno66@gmail.com>
2026-03-06 13:49:18 +0800

    feat: create Toppin model

--- chapter_18/pizzeria/pizzas/models.py
+++ chapter_18/pizzeria/pizzas/models.py
@@ -7,3 +7,18 @@ class Pizza(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.name
+
+class Topping(models.Model):
+    """Something specific learned about a topic."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.text) > 50:
+            return f"{self.pizza.name}: {self.name[:50]}..."
+        
+        return f"{self.pizza.name}: {self.name}"
