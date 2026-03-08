a91c99a3bbc3c91fcc50df43a2acdfeecf8ff50e
Jules <julesmoreno66@gmail.com>
2026-03-08 16:44:35 +0800

    fix: remove unused topic_id parameter from topics view

--- chapter_18/learning_log/learning_logs/views.py
+++ chapter_18/learning_log/learning_logs/views.py
@@ -6,7 +6,7 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
-def topics(request, topic_id):
+def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
