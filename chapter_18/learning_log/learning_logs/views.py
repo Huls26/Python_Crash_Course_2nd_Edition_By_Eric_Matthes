a16f9c1a277dc68878c1b87402c2312ffe3da2af
Jules <julesmoreno66@gmail.com>
2026-03-08 16:36:22 +0800

    refactor: update topics view and add topic detail view

--- chapter_18/learning_log/learning_logs/views.py
+++ chapter_18/learning_log/learning_logs/views.py
@@ -7,6 +7,12 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 def topics(request, topic_id):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
